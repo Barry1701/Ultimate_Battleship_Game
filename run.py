8efa55d431d5ff2e3beedf0a69f7883ab4274eb6
Barry1701 <>
2023-11-13 12:20:13 +0000

    Add new_game function responsible for setting up a new game.

--- run.py
+++ run.py
@@ -39,8 +39,52 @@ class Board:
             if self.type == "player":
                 self.board [x][y] = "@"
 
-    def random_point(size):
+def random_point(size):
         """
         Helper function to return random integer between o and size
         """
-        return randint(0, size-1)               
+        return randint(0, size - 1)
+
+def valid_coordinates(x, y, board)
+
+
+def populate_board(board)
+
+
+def make_guess(board)
+
+
+def play_game(computer_board, player_board)
+
+
+def new_game():
+        """
+        Starts a game. Sets the board size and number of ships.
+        Resets the scores and initialises the board.
+        """                   
+        size = 5
+        num_ships = 4
+        scores["computer"] = 0
+        scores["player"] = 0
+        print("-" * 38)
+        print("Welcome to the ULTIMATE BATTLESHIPS")
+        print(f"Board Size: {size}. Number Of the Ships: {num_ships}")
+        print("Top left corner is Row: 0, Column: 0")
+        print("-" * 38)
+        player_name = input("Please enter your name: \n")
+        print("-" * 38)
+
+        computer_board = Board(size, num_ships, "Computer", type= "computer")
+        player_board = Board(size, num_ships, player_name, type= "player")
+
+        for _ in range(num_ships):
+            populate_board(player_board)
+            populate_board(computer_board)
+        
+        play_game(computer_board, player_board)
+
+new_game()
+
+
+    
+    
